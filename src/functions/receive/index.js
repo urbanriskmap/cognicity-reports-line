aac2f3635a36ba3bc96ec4b317468f540c41d81c
abrahamq <abrahamquintero2323@gmail.com>
2018-12-07 14:18:32 -0500

    remove signature check on reply

--- src/functions/receive/index.js
+++ src/functions/receive/index.js
@@ -4,14 +4,18 @@ const crypto = require('crypto');
 
 export default (event, context, callback) => {
 // exports.handler = function (event, context) {
+ console.log('Got webhook request');
+ console.log(event);
  let signature = crypto.createHmac('sha256', config.channelSecret).update(event.body).digest('base64');
  let checkHeader = (event.headers || {})['X-Line-Signature'];
  
  if (signature === checkHeader) {
+   console.log('Processing webhook message');
    const linebot = new Linebot(config);  
        linebot.processMessage(event)
  }else{
-   console.log('Error');
+   console.log('Error, signature does not match header');
+   console.log(event);
  }
 };
 
--- src/functions/send/index.js
+++ src/functions/send/index.js
@@ -3,18 +3,10 @@ import config from '../../config';
 const crypto = require('crypto');
 
 export default (event, context, callback) => {
+  // authentication is done via x-api-key on AWS API gateway
   console.log('Got send request');
   console.log(event);
-  let signature = crypto.createHmac('sha256', config.channelSecret).update(event.body).digest('base64');
-  let checkHeader = (event.headers || {})['X-Line-Signature'];
-
-  if (signature === checkHeader) {
-    console.log('Signature matches, sending thanks!');
-    const linebot = new Linebot(config);  
-    linebot.sendThanks(event)
-  }else{
-    console.log('Error, signature does not match header');
-    console.log(event);
-  }
+  const linebot = new Linebot(config);
+  linebot.sendThanks(event)
 };
 
