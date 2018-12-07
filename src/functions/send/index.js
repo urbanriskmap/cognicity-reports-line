9ee73fce772cb364900756e04a3a505644f7682c
abrahamq <abrahamquintero2323@gmail.com>
2018-12-07 14:07:04 -0500

    add more logging

--- src/functions/send/index.js
+++ src/functions/send/index.js
@@ -3,16 +3,18 @@ import config from '../../config';
 const crypto = require('crypto');
 
 export default (event, context, callback) => {
- let signature = crypto.createHmac('sha256', config.channelSecret).update(event.body).digest('base64');
- let checkHeader = (event.headers || {})['X-Line-Signature'];
- 
- if (signature === checkHeader) {
-   const linebot = new Linebot(config);  
-       linebot.sendThanks(event)
- }else{
-   console.log('Error');
- }
+  console.log('Got send request');
+  console.log(event);
+  let signature = crypto.createHmac('sha256', config.channelSecret).update(event.body).digest('base64');
+  let checkHeader = (event.headers || {})['X-Line-Signature'];
+
+  if (signature === checkHeader) {
+    console.log('Signature matches, sending thanks!');
+    const linebot = new Linebot(config);  
+    linebot.sendThanks(event)
+  }else{
+    console.log('Error, signature does not match header');
+    console.log(event);
+  }
 };
-
-
 
